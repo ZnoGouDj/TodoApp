8649afb9a36521c5295223df7bc2ce0d88ddfcc9
Znogoud <znogoud@pop-os.localdomain>
2021-10-12 16:19:25 +0300

    Add item status filter functionality

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -4,30 +4,33 @@ import './item-status-filter.css';
 
 export default class ItemStatusFilter extends Component {
 
-  state = {
-    toggle: [
-      { name: 'All', active: true, id: 1 },
-      { name: 'Active', active: false, id: 2 },
-      { name: 'Done', active: false, id: 3 }
-    ]
-  }
-
-  onSwitch = (e) => {
-    
-
-
-  }
+  buttons = [
+    { name: 'all', label: 'All' },
+    { name: 'active', label: 'Active' },
+    { name: 'done', label: 'Done' }
+  ]
 
   render() {
+
+    const { filter, onFilterChange } = this.props;
+
+    const buttons = this.buttons.map(({ name, label }) => {
+      const isActive = filter === name;
+      const clazz = isActive ? 'btn-info' : 'btn-outline-secondary';
+      return (
+        <button type="button"
+          className={`btn ${clazz}`}
+          key={name}
+          onClick={() => onFilterChange(name)}>
+          {label}
+        </button>
+      )
+    })
+
     return (
-      <form className="btn-group" onClick={this.onSwitch}>
-        <button type="button"
-          className="btn btn-info" >All</button>
-        <button type="button"
-          className="btn btn-outline-secondary">Active</button>
-        <button type="button"
-          className="btn btn-outline-secondary">Done</button>
-      </form>
+      <div className="btn-group">
+        {buttons}
+      </div>
     );
   }
 }
